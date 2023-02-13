cf2fef7794ed32412ef74da445df1a84133164c9
betulturkut <btlturkut@gmail.com>
2023-02-13 11:19:50 +0300

    Singleton Pattern Desing

---
+++ SingletonPatternDesign/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPatternDesing
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Save();
+        }
+
+        class CustomerManager
+        {
+            private static CustomerManager _customerManager;
+            private CustomerManager()
+            {
+
+            }
+
+            public static CustomerManager CreateAsSingleton()
+            {
+                return _customerManager ?? (_customerManager = new CustomerManager());
+            }
+
+            public void Save()
+            {
+                Console.WriteLine("Saved!");
+            }
+        }
+    }
+}
