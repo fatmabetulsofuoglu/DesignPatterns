04986568eaab8d6f86cf4d975a85b2f31f0217c4
betulturkut <btlturkut@gmail.com>
2023-02-13 11:58:17 +0300

    Factory Design Pattern

--- FactoryDesignPattern/Program.cs
+++ FactoryDesignPattern/Program.cs
@@ -10,6 +10,62 @@ namespace FactoryDesignPattern
     {
         static void Main(string[] args)
         {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory());
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    public class LoggerFactory : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            // Business to decide factory
+            return new ExamplesLogger();
+        }
+    }
+
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            // Business to decide factory
+            return new ExamplesLogger();
+        }
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public class ExamplesLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with ExampleLogger!");
+        }
+    }
+
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
         }
     }
 }
