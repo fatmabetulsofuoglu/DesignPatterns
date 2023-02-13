8f6e5d76e9609a85a1075239dc614f6d3da3df2d
betulturkut <btlturkut@gmail.com>
2023-02-13 11:28:43 +0300

    Factory Design Pattern

---
+++ SingletonDesingPattern/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesingPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Save();
+        }
+
+        class CustomerManager
+        {
+            private static CustomerManager _customerManager;
+            private CustomerManager()
+            {
+
+            }
+
+            public static CustomerManager CreateAsSingleton()
+            {
+                return _customerManager ?? (_customerManager = new CustomerManager());
+            }
+
+            public void Save()
+            {
+                Console.WriteLine("Saved!");
+            }
+        }
+    }
+}
